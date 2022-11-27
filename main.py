a7307663146228b25983b61e250b104a1e255e62
Rafa ✏️ <adanescollante@gmail.com>
2022-11-26 22:56:55 -0500

    redid all python code

--- main.py
+++ main.py
@@ -1,34 +1,33 @@
-# Declara variables
-pp = "primer parcial"
-sp = "segundo parcial"
-ef = "examen final"
-pt = "práctica total"
-
-# Crea arrays para ahorrar líneas de código
-variables = [pp, sp, ef, pt]
-calificaciones = []
-
-# Pide al usuario digitar calificaciones obtenidas y las guarda en array calificaciones[]
-def digiteCalificaciones():
-  i = 1
-  for x in variables:
-    calificaciones.append(input("Digite la calificación de su " + x + ": "))
-    i += 1
-    if i > 4:
-      break
-
-# Calcula el promedio parcial y la calificación final obtenidos
-def calcularCalificacionFinal():
-  totalParcial = (int(calificaciones[0]) + int(calificaciones[1])) / 2
-  calificacionFinal = (totalParcial + int(calificaciones[2]) + int(calificaciones[3])) / 3
-  print("El estudiante pasó la materia en", literal, ", y su calificación final fue de", round(calificacionFinal), ".")
+def calcular():
+  literal = ""
+  estatus = ""
+  pp = int(input("Digite la calificación de su primer parcial" + ": "))
+  sp = int(input("Digite la calificación de su segundo parcial" + ": "))
+  ef = int(input("Digite la calificación de su examen final" + ": "))
+  tp = int(input("Digite la calificación de su total práctica" + ": "))
+  cf = round((((pp + sp) / 2) + ef + tp) / 3)
+  if cf > 90:
+    literal = "A"
+    estatus = "APROBADO"
+  elif cf > 80:
+    literal = "B"
+    estatus = "APROBADO"
+  elif cf > 70:
+    literal = "C"
+    estatus = "APROBADO"
+  elif cf > 60:
+    literal = "D"
+    estatus = "REPROBADO"
+  else:
+    literal = "F"
+    estatus = "REPROBADO"
+  print("El estudiante " + estatus + " la materia en " + literal + " porque su calificación fue de", cf)
 
 def main():
   print('------------------------------')
   print('--Calculador de calificación--')
   print('------------final-------------')
   print('------------------------------')
-  digiteCalificaciones()
-  calcularCalificacionFinal()
+  calcular()
 
 main()
